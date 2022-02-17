f7cf2049cff07fd4bfdb70f9c9af13296f5630c4
guofei <guogoffy@outlook.com>
2022-02-17 16:44:38 +0800

    Initial commit

---
+++ src/main/java/DownloaderApplication.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: GuoFei
+ * @Date: 2022/02/17/16:39
+ * @Description:
+ */
+public class DownloaderApplication {
+    public static void main(String[] args) {
+        String url = null;
+        if (args == null || args.length == 0) {
+            for (; ; ) {
+                System.out.println("请输入下载链接");
+                Scanner scanner = new Scanner(System.in);
+                url = scanner.next();
+                if (url != null) {
+                    break;
+                }
+            }
+        } else {
+            url = args[0];
+        }
+        System.out.println(url);
+    }
+}
